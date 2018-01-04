94587f28bf95321b4ca241f1db79de33088422cc
GuyXY <guyxy@yandex.com>
2018-01-04 20:40:37 +0100

    stopped deleting the VISITOR_COOKIE as that stops the YouTube comments from loading

--- bgscript.js
+++ bgscript.js
@@ -1,39 +1,13 @@
-
-async function removeVisitorCookie(cookie) {
-	await browser.cookies.remove(createCookieInfo(cookie));
-	console.log(`removed ${cookie.name}`);
-}
-
-async function removeVisitorCookies() {
-	for(let cookieStore of await browser.cookies.getAllCookieStores()) {
-		removeVisitorCookie({
-			"name": VISITOR_COOKIE_NAME,
-			"storeId": cookieStore.id
-		});
-	}
-}
-
-//as soon as the add-on gets startet, it's going to delete the VISITOR cookie aand patch the PREF cookie
+//as soon as the add-on gets startet, it's going to patch the PREF cookie
 (async function() {
 	setPrefCookies((await browser.storage.sync.get("status")).status);
-	removeVisitorCookies();
 })();
 
 //automatically patch the PREF cookie as soon as it gets changed
 browser.cookies.onChanged.addListener(async function(changedInfo) {
 	let {cookie} = changedInfo;
 
-	switch(cookie.name) {
-		case PREF_COOKIE_NAME:
-			if(changedInfo.cause != "overwrite") {
-				setPrefCookie(cookie, (await browser.storage.sync.get("status")).status, changedInfo.removed)
-			}
-			break;
-
-		//this cookie can break the PREF changes, so it will be removed
-		case VISITOR_COOKIE_NAME:
-			if(!changedInfo.removed) {
-				removeVisitorCookie(cookie);
-			}
+	if(cookie.name == PREF_COOKIE_NAME && changedInfo.cause != "overwrite") {
+		setPrefCookie(cookie, (await browser.storage.sync.get("status")).status, changedInfo.removed);
 	}
 });
