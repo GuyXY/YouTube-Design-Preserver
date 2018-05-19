39c430075bc84fe9ab392a595a02c4b0777ca80e
GuyXY <guyxy@mail.ru>
2018-05-19 23:40:34 +0200

    restructured repository and adjusted build script

--- build.sh
+++ build.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-DIR=$PWD
-COMMON_FILES="icons lib _locales settings bgscript.js shared.js"
+DIST_DIR=$PWD/dist
+WEBEXT_POLYFILL=$PWD/webextension-polyfill/dist/browser-polyfill.min.js
 
-mkdir -p gen/firefox
-mkdir -p gen/chromeBased/settings
+rm -rf $DIST_DIR
+mkdir -p $DIST_DIR/firefox $DIST_DIR/chrome
 
-cp -r $COMMON_FILES gen/firefox/
-cp -r $COMMON_FILES gen/chromeBased/
+cp -r src/. $DIST_DIR/firefox/
+cp -r src/. $DIST_DIR/chrome/
 
-cp webextension-polyfill/dist/browser-polyfill.min.js gen/chromeBased/lib/
+# if the webextension polyfill is not yet initialized, do it now
+if [ ! -e $WEBEXT_POLYFILL ]; then
 
-node generator.js
+	git clone https://github.com/mozilla/webextension-polyfill
+	cd webextension-polyfill
+	
+	npm install
+	npm run build
 
-cd $DIR/gen/firefox
-zip -r $DIR/firefox.zip *
-cd $DIR/gen/chromeBased
-zip -r $DIR/chromeBased.zip *
+fi
+
+cp webextension-polyfill/dist/browser-polyfill.min.js $DIST_DIR/chrome/lib/
+
+jq -c '. + {"applications": {"gecko": {"id": "{5b7175f9-183b-4421-b105-82ef7ef426d0}"}}}' < manifest.json > $DIST_DIR/firefox/manifest.json
+jq -c '.background.scripts |= ["lib/browser-polyfill.min.js"] + .' < manifest.json > $DIST_DIR/chrome/manifest.json
+
+sed -i '/<\/head>/s|^|<script src="/lib/browser-polyfill.min.js"></script>|' $DIST_DIR/chrome/settings/settings.html
+
+cd $DIST_DIR/firefox;   zip -r $DIST_DIR/firefox.zip *
+cd $DIST_DIR/chrome;    zip -r $DIST_DIR/chrome.zip *
--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install jsdom
-
-git clone https://github.com/mozilla/webextension-polyfill
-cd webextension-polyfill
-
-npm install
-npm run build
