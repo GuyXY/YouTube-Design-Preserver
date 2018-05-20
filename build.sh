61ee7c562ef1fc99a2eba381de633325a462b6fd
GuyXY <guyxy@mail.ru>
2018-05-20 05:39:11 +0200

    fixed bug in build script and added comments as well as "set -e"

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# make sure that the script exits as soon as an error occurs
+set -e
+
+# Variables
 DIST_DIR=$PWD/dist
 WEBEXT_POLYFILL=$PWD/webextension-polyfill/dist/browser-polyfill.min.js
 
+# cleanup dist directory and recreate the needed directories
 rm -rf $DIST_DIR
 mkdir -p $DIST_DIR/firefox $DIST_DIR/chrome
 
+# copy the source files into the firefox and chrome dist folders
 cp -r src/. $DIST_DIR/firefox/
 cp -r src/. $DIST_DIR/chrome/
 
@@ -17,15 +23,21 @@ if [ ! -e $WEBEXT_POLYFILL ]; then
 	
 	npm install
 	npm run build
+	
+	cd ..
 
 fi
 
+# add the webextension polyfill for all chromium based browsers
 cp webextension-polyfill/dist/browser-polyfill.min.js $DIST_DIR/chrome/lib/
 
+# patch the manifest files accordingly
 jq -c '. + {"applications": {"gecko": {"id": "{5b7175f9-183b-4421-b105-82ef7ef426d0}"}}}' < manifest.json > $DIST_DIR/firefox/manifest.json
 jq -c '.background.scripts |= ["lib/browser-polyfill.min.js"] + .' < manifest.json > $DIST_DIR/chrome/manifest.json
 
+# add an include for the webextension polyfill to the settings html file
 sed -i '/<\/head>/s|^|<script src="/lib/browser-polyfill.min.js"></script>|' $DIST_DIR/chrome/settings/settings.html
 
+# create the final zip files for every browser
 cd $DIST_DIR/firefox;   zip -r $DIST_DIR/firefox.zip *
 cd $DIST_DIR/chrome;    zip -r $DIST_DIR/chrome.zip *
